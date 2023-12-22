4daa516e64de0c653086aaf4a95d4fe052e9e7f5
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-22 22:52:50 +0100

    user saved - make migration

--- migrations/Version20231222215054.php
+++ migrations/Version20231222215054.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231222212449 extends AbstractMigration
+final class Version20231222215054 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,7 @@ final class Version20231222212449 extends AbstractMigration
         $this->addSql('CREATE TABLE label (id INT AUTO_INCREMENT NOT NULL, whish_list_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_EA750E81B35BBAD (whish_list_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE label_proposition (label_id INT NOT NULL, proposition_id INT NOT NULL, INDEX IDX_2C1113A533B92F39 (label_id), INDEX IDX_2C1113A5DB96F9E (proposition_id), PRIMARY KEY(label_id, proposition_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE proposition (id INT AUTO_INCREMENT NOT NULL, creator_id INT DEFAULT NULL, wish_list_id INT DEFAULT NULL, content LONGTEXT NOT NULL, difficulty INT NOT NULL, INDEX IDX_C7CDC35361220EA6 (creator_id), INDEX IDX_C7CDC353D69F3311 (wish_list_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(255) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE wish_list (id INT AUTO_INCREMENT NOT NULL, creator_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_5B8739BD61220EA6 (creator_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE wish_list_user (wish_list_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_8F75446D69F3311 (wish_list_id), INDEX IDX_8F75446A76ED395 (user_id), PRIMARY KEY(wish_list_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
