29fab37f567a7508e9c7f6267ccf936145422b9e
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-23 09:46:30 +0100

    fix webpack config problems ??? and creatue user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername('Nicolas');
+        $user->setEmail('nico@nico.com');
+        $user->setRoles(['ROLE_USER']);
+        $password = 'plop';
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
