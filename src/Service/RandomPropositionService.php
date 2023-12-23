fa62ab141915aeab594c4f6159624fb8956057e7
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-23 19:21:14 +0100

    add RandomPropositionService

---
+++ src/Service/RandomPropositionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Proposition;
+use App\Entity\WishList;
+
+class RandomPropositionService
+{
+    public function getRandomProposition(WishList $wishList): Proposition
+    {
+
+    }
+}
