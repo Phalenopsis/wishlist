81c17e77f4994e910f2c7cb9a2e1dea03b003f09
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-29 20:48:56 +0100

    change labels choice

--- src/Form/ChangeLabelsType.php
+++ src/Form/ChangeLabelsType.php
@@ -10,6 +10,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ChangeLabelsType extends AbstractType
@@ -17,13 +19,31 @@ class ChangeLabelsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('labels', EntityType::class, [
-                'class' => Label::class,
-                'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => true,
-                'by_reference' => false,
-            ])
+//            ->add('labels', EntityType::class, [
+//                'class' => Label::class,
+//                'choice_label' => 'name',
+//                'multiple' => true,
+//                'expanded' => true,
+//                'by_reference' => false,
+//            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+                $proposition = $event->getData();
+                $form = $event->getForm();
+//                $form->add('labels', TextType::class, [
+//                   'attr' => ['placeholder' => $proposition->getWishList()->getName()]
+//                ]);
+//
+                if(count($proposition->getWishList()->getLabels()) > 0){
+                    $form->add('labels', EntityType::class, [
+                        'class' => Label::class,
+                        'choices' => $proposition->getWishList()->getLabels(),
+                        'choice_label' => 'name',
+                        'multiple' => true,
+                        'expanded' => true,
+                        'by_reference' => false,
+                    ]);
+                }
+            })
         ;
     }
 
