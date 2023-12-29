e8e035d7c7b9c37a87902d750be85f66bf2cb518
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-29 20:48:46 +0100

    change labels choice

--- src/Form/PropositionType.php
+++ src/Form/PropositionType.php
@@ -9,8 +9,13 @@ use App\Entity\WishList;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class PropositionType extends AbstractType
 {
@@ -24,6 +29,24 @@ class PropositionType extends AbstractType
                     'max' => 5
                 ]
             ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+                $proposition = $event->getData();
+                $form = $event->getForm();
+//                $form->add('labels', TextType::class, [
+//                   'attr' => ['placeholder' => $proposition->getWishList()->getName()]
+//                ]);
+//
+                if(count($proposition->getWishList()->getLabels()) > 0){
+                    $form->add('labels', EntityType::class, [
+                        'class' => Label::class,
+                        'choices' => $proposition->getWishList()->getLabels(),
+                        'choice_label' => 'name',
+                        'multiple' => true,
+                        'expanded' => true,
+                        'by_reference' => false,
+                    ]);
+                }
+            })
 //            ->add('creator', EntityType::class, [
 //                'class' => User::class,
 //'choice_label' => 'id',
@@ -31,14 +54,21 @@ class PropositionType extends AbstractType
 //            ->add('wish_list', EntityType::class, [
 //                'class' => WishList::class,
 //'choice_label' => 'id',
+////            ])
+//            ->add('labels', EntityType::class, [
+//                'class' => Label::class,
+//                'choice_label' => 'name',
+//                'multiple' => true,
+//                'expanded' => true,
+//                'by_reference' => false,
 //            ])
-            ->add('labels', EntityType::class, [
-                'class' => Label::class,
-                'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => true,
-                'by_reference' => false,
-            ])
+//            ->add('labels', ChoiceType::class, [
+//                'choices'  => [
+//                    //on met ici l'array de label de wishlist
+//                    'Maybe' => null,
+//                    'Yes' => true,
+//                    'No' => false,
+//                ],])
         ;
     }
 
