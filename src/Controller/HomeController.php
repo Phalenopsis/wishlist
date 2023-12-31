0fb17ea7f09b80883fad814cf92c5fd20a4dd2f8
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-31 16:09:39 +0100

    add 2 methods to test links and doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,4 +17,21 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/test', name: 'app_home_test')]
+    public function testLink(): Response
+    {
+        return $this->render('home/test.html.twig');
+    }
+
+    #[Route('/test/doctrine', name: 'app_home_test_doctrine')]
+    public function testLinkDoctrine(UserRepository $repository): Response
+    {
+        $users = $repository->findAll();
+        return $this->render('home/testdoctrine.html.twig', [
+            'users' => $users,
+                ]
+            );
+    }
+
 }
